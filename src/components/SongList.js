b51e931fc93285d6d3e4f596e808a2863ffed3f8
Denny <denny1011133@luci.group>
2022-02-11 11:26:24 +0800

    show songList

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -2,10 +2,21 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class SongList extends Component {
-  render() {
-    console.log(this.props);
+  renderList() {
+    return this.props.songs.map(song => {
+      return (
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <div className="ui button primary">Select</div>
+          </div>
+          <div className="content">{song.title}</div>
+        </div>
+      );
+    });
+  }
 
-    return <div>SongList</div>;
+  render() {
+    return <div className="ui divided list">{this.renderList()}</div>;
   }
 }
 
@@ -14,4 +25,3 @@ const mapStateToProps = state => {
 };
 
 export default connect(mapStateToProps)(SongList);
-
--- src/index.js
+++ src/index.js
@@ -5,11 +5,9 @@ import { createStore } from 'redux';
 import App from './components/App';
 import reducers from './reducers';
 
-
 ReactDOM.render(
     <Provider store={createStore(reducers)}>
         <App />
     </Provider>,
     document.querySelector('#root')
 );
-
